d4a9afe93499b915ddc8a82b3f57274ba84c9a9b
logicalmechanism <logical.mechanism@protonmail.com>
2023-08-05 10:14:12 -0700

    cleaned up code and added comments

--- tx_simulation.py
+++ tx_simulation.py
@@ -9,6 +9,16 @@ import cbor2
 import requests
 
 
+def to_bytes(s: str) -> bytes:
+    # Convert the string to bytes and prepend with 'h' to indicate hexadecimal format
+    return bytes.fromhex(s)
+
+
+def tx_draft_to_resolved(draft: str) -> list:
+    # return the data from the cbor for parsing
+    return cbor2.loads(bytes.fromhex(draft))
+
+
 def query_tx_with_koios(hashes: list, network: bool) -> list:
     """Uses Koios to query the transaction information from a list of
     transaction hashes. The return order may not match the input order.
@@ -26,7 +36,7 @@ def query_tx_with_koios(hashes: list, network: bool) -> list:
     json_data = {
         '_tx_hashes': hashes
     }
-    
+
     headers = {
         'accept': 'application/json',
         'content-type': 'application/json',
@@ -35,17 +45,8 @@ def query_tx_with_koios(hashes: list, network: bool) -> list:
     return requests.post('https://' + subdomain + '.koios.rest/api/v0/tx_info', headers=headers, json=json_data).json()
 
 
-def to_bytes(s: str) -> bytes:
-    # Convert the string to bytes and prepend with 'h' to indicate hexadecimal format
-    return bytes.fromhex(s)
-
-def tx_draft_to_resolved(draft: str) -> list:
-    # return the data from the cbor for parsing
-    return cbor2.loads(bytes.fromhex(draft))
-
-
-def from_file(tx_draft_path: str, network: bool, debug: bool = False) -> dict:
-    """Simulate a tx from a tx draft file for some network. 
+def from_file(tx_draft_path: str, network: bool, debug: bool = False, aiken_path: str = 'aiken') -> dict:
+    """Simulate a tx from a tx draft file for some network.
 
     Args:
         tx_draft_path (str): The path to the tx.draft file.
@@ -58,10 +59,10 @@ def from_file(tx_draft_path: str, network: bool, debug: bool = False) -> dict:
     with open(tx_draft_path, 'r') as file:
         data = json.load(file)
     cborHex = data['cborHex']
-    return from_cbor(cborHex, network, debug)
+    return from_cbor(cborHex, network, debug, aiken_path)
 
 
-def from_cbor(tx_cbor: str, network: bool, debug: bool = False) -> dict:
+def from_cbor(tx_cbor: str, network: bool, debug: bool = False, aiken_path: str = 'aiken') -> dict:
     """Simulate a tx from tx cbor for some network. 
 
     Args:
@@ -76,27 +77,27 @@ def from_cbor(tx_cbor: str, network: bool, debug: bool = False) -> dict:
 
     # we just need the body here
     txBody = data[0]
-    
+
     # all the types of inputs; tx inputs, collateral, and reference
     inputs = txBody[0] + txBody[13] + txBody[18]
     input_cbor = cbor2.dumps(inputs).hex()
-    
+
     # we now need to loop all the input hashes to resolve their outputs
     tx_hashes = [x[0].hex() for x in inputs]
     result = query_tx_with_koios(tx_hashes, network)
-    
+
     # build out the list of resolved input outputs
     outputs = []
     # the order of the resolved outputs matter so we match to the inputs
     for utxo in inputs:
         tx_hash = utxo[0].hex()
-        
+
         # now find the result for that hash
         for tx in result:
             this_tx_hash = tx['tx_hash']
             if tx_hash != this_tx_hash:
                 continue
-            
+
             resolved = {}
             for utxo in tx['outputs']:
                 idx = utxo['tx_index']
@@ -104,7 +105,7 @@ def from_cbor(tx_cbor: str, network: bool, debug: bool = False) -> dict:
                 # we found it
                 if [to_bytes(tx_hash), idx] in inputs:
                     # lets build out the resolved output
-                    
+
                     # assume that anything with a datum is a contract
                     if utxo['inline_datum'] != None:
                         network_flag = "71" if network is True else "70"
@@ -121,44 +122,48 @@ def from_cbor(tx_cbor: str, network: bool, debug: bool = False) -> dict:
                         pkh = network_flag + utxo['payment_addr']['cred']
                         pkh = to_bytes(pkh)
                         resolved[0] = pkh
-                    
+
                     if utxo['reference_script'] != None:
                         # assume plutus v2 reference scripts
                         cbor_ref = to_bytes(utxo['reference_script']['bytes'])
                         cbor_ref = to_bytes(cbor2.dumps([2, cbor_ref]).hex())
+
+                        # put the reference script in the correct format
                         resolved[3] = cbor2.CBORTag(24, cbor_ref)
-                    
+
                     # now we need the value element
                     lovelace = utxo['value']
                     assets = utxo['asset_list']
-                    
-                    # its just an int for lovelace
+
+                    # its just an int when its lovelace only
                     if len(assets) == 0:
-                        second = int(lovelace)
+                        value = int(lovelace)
                     else:
                         # build out the assets then create the asset list
                         tkns = {}
                         for asset in assets:
                             tkns[to_bytes(asset['policy_id'])] = {}
-                            tkns[to_bytes(asset['policy_id'])][to_bytes(asset['asset_name'])] = int(asset['quantity'])
+                            tkns[to_bytes(asset['policy_id'])][to_bytes(
+                                asset['asset_name'])] = int(asset['quantity'])
+
                         # the form for assets is a list
-                        second = [int(lovelace), tkns]
-                    resolved[1] = second
-                    
+                        value = [int(lovelace), tkns]
+                    resolved[1] = value
+
                     # append it and go to the next one
                     outputs.append(resolved)
                     break
-    
+
     # get the resolved output cbor
     output_cbor = cbor2.dumps(outputs).hex()
-    
+
     # attempt to debug if required
     if debug is True:
-        print(tx_cbor,'\n')
-        print(input_cbor,'\n')
-        print(output_cbor,'\n')
-    
-    # try to simulate the tx and return the results else an empty dict
+        print(tx_cbor, '\n')
+        print(input_cbor, '\n')
+        print(output_cbor, '\n')
+
+    # try to simulate the tx and return the results else return an empty dict
     try:
         # use some temp files that get deleted later
         with tempfile.NamedTemporaryFile(mode='w+', delete=False) as temp_tx_file:
@@ -170,43 +175,49 @@ def from_cbor(tx_cbor: str, network: bool, debug: bool = False) -> dict:
         with tempfile.NamedTemporaryFile(mode='w+', delete=False) as temp_output_file:
             temp_output_file.write(output_cbor)
             temp_output_file_path = temp_output_file.name
-                    
+
+        # the default value assumes aiken to be on path
+        # or it uses the aiken path
         output = subprocess.run(
-            ['aiken', 'tx', 'simulate', temp_tx_file_path, temp_input_file_path, temp_output_file_path],
+            [aiken_path, 'tx', 'simulate', temp_tx_file_path,
+                temp_input_file_path, temp_output_file_path],
             check=True,
-            capture_output=True, 
+            capture_output=True,
             text=True
         )
-            
+
+        # this should remove the temp files
         os.remove(temp_tx_file_path)
         os.remove(temp_input_file_path)
         os.remove(temp_output_file_path)
-        
+
         return json.loads(output.stdout)
     except subprocess.CalledProcessError as e:
+        # the simulation failed in some way
         return {}
-        
+
+
 if __name__ == "__main__":
     """
-    
+
     Pass in some tx.draft file that the cardano-cli created into the tx simulation
     or pass in the cbor from the tx file.
-    
+
     """
     main_script_dir = os.getcwd()
     target_folder = os.path.join(main_script_dir, 'test_data')
-    
+
     network = False
     debug = False
-    
+
     #
     # Single tx draft test
     #
-    
+
     # tx_draft_path = target_folder+"/tx1.draft"
     # required_units = from_file(tx_draft_path, network, debug)
     # print(required_units)
-    
+
     #
     # All tx draft test
     #
@@ -215,7 +226,5 @@ if __name__ == "__main__":
         print(os.path.basename(f))
         required_units = from_file(f, network, debug)
         print(required_units)
+        # sleep to not hit the rate limit
         sleep(0.1)
-   
-        
-    
